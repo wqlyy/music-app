cc099200d236035ae6d43cc410bbeed86cd9263c
wangqiang <wangqiangwen@163.com>
2018-02-27 17:35:05 +0800

    添加sass支持，环境基础搭建

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        hello world
+    </div>
+</template>
+
+<script>
+import 'normalize.css'
+import './test.scss'
+export default {
+//   data(){
+//     return {}
+//   }
+}
+</script>
