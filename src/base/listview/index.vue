f5e2ad8b18823c5fe4d76579b7144f9c6c6c3535
wangqiang <wangqiangwen@163.com>
2018-03-01 16:02:17 +0800

    右侧列表

--- src/base/listview/index.vue
+++ src/base/listview/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <scroll class="list-view" :data="data">
+  <scroll class="list-view" :data="data" ref='listview'>
     <ul>
-      <li v-for="(group,index) in data" :key="index" class="list-group">
+      <li v-for="(group,index) in data" :key="index" class="list-group" ref="listGroup">
         <h2 class="list-group-title">
           {{group.title}}
         </h2>
@@ -13,13 +13,25 @@
         </ul>
       </li>
     </ul>
+    <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
+      <ul>
+        <li v-for="(item, index) in shortcutList" :data-index="index" :key="index" class="item">{{item}}</li>
+      </ul>
+    </div>
   </scroll>
 </template>
 
 <script>
 import Scroll from '@/base/scroll'
+import {getData} from '@/common/js/dom'
 import './index.scss'
+
+const ANCHOR_HEIGHT = 18
+
 export default {
+  created() {
+    this.touch = {}
+  },
   components: {
     Scroll
   },
@@ -27,6 +39,32 @@ export default {
     data: {
       type: Array,
       default: null
+    }
+  },
+  computed: {
+    shortcutList() {
+      return this.data.map((group) => {
+        return group.title.substr(0, 1)
+      })
+    }
+  },
+  methods: {
+    onShortcutTouchStart(e) {
+      let anchorIndex = getData(e.target, 'index');
+      let firstTouch = e.touches[0];
+      this.touch.y1 = firstTouch.pageY;
+      this.touch.anchorIndex = anchorIndex;
+      this._scrollTo(anchorIndex)
+    },
+    onShortcutTouchMove(e){
+      let firstTouch = e.touches[0];
+      this.touch.y2 = firstTouch.pageY;
+      let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0;
+      let anchorIndex = (parseInt(this.touch.anchorIndex)) + delta;
+      this._scrollTo(anchorIndex)
+    },
+    _scrollTo(index) {
+      this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
     }
   }
 }
