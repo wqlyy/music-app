1885ea379db184afa56c3572a006219a46b185b7
wangqiang <wangqiangwen@163.com>
2018-03-01 17:13:07 +0800

    歌手列表，细节处理

--- src/base/listview/index.vue
+++ src/base/listview/index.vue
@@ -30,15 +30,23 @@
         </li>
       </ul>
     </div>
+    <div class="list-fixed" ref="fixed" v-show="fixedTitle">
+      <h1 class="fixed-title">{{fixedTitle}}</h1>
+    </div>
+    <div v-show="!data.length" class="loading-container">
+      <loading></loading>
+    </div>
   </scroll>
 </template>
 
 <script>
 import Scroll from '@/base/scroll'
+import Loading from '@/base/loading'
 import {getData} from '@/common/js/dom'
 import './index.scss'
 
 const ANCHOR_HEIGHT = 18
+const TITLE_HEIGHT = 30
 
 export default {
   created() {
@@ -48,12 +56,14 @@ export default {
     this.probeType = 3
   },
   components: {
-    Scroll
+    Scroll,
+    Loading
   },
   data() {
     return {
       scrollY: -1,
-      currentIndex: 0
+      currentIndex: 0,
+      diff: -1
     }
   },
   props: {
@@ -67,6 +77,12 @@ export default {
       return this.data.map((group) => {
         return group.title.substr(0, 1)
       })
+    },
+    fixedTitle() {
+      if (this.scrollY > 0) {
+        return ''
+      }
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
     }
   },
   methods: {
@@ -129,13 +145,22 @@ export default {
         let height1 = listHeight[i]
         let height2 = listHeight[i + 1]
         if (-newY >= height1 && -newY < height2) {
-          this.currentIndex = i
+          this.currentIndex = i;
+          this.diff = height2 + newY
           // console.log(this.currentIndex)
           return
         }
       }
       // 当滚动到底部，且-newY大于最后一个元素的上线
       this.currentIndex = listHeight.length - 2
+    },
+    diff(newVal) {
+      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+      if (this.fixedTop === fixedTop) {
+        return
+      }
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0, ${fixedTop}px, 0)`
     }
   }
 }
