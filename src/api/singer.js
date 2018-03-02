43e16eb05782e03d7fd66b01364d6e017b015da5
wangqiang <wangqiangwen@163.com>
2018-03-02 12:44:23 +0800

    歌曲数据抓取及处理

--- src/api/singer.js
+++ src/api/singer.js
@@ -17,3 +17,30 @@ export function getSingerList() {
   })
   return jsonp(url, data, options)
 }
+
+export function getSingerDetail(singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg';
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    order: 'listen',
+    begin: 0,
+    num: 100,
+    songstatus: 1,
+    singermid: singerId,
+    g_tk: 5381
+  })
+  return jsonp(url, data, options)
+}
+
+export function getPlayKey() {
+  const url = 'http://base.music.qq.com/fcgi-bin/fcg_musicexpress.fcg';
+  const data = Object.assign({}, commonParams, {
+    json: 3,
+    loginUin: 0,
+    platform: 'yqq',
+    needNewCode: 0
+  })
+  return jsonp(url, data, options)
+}
---
+++ src/common/js/song.class.js
@@ -0,0 +1,47 @@
+import {getPlayKey} from '@/api/singer'
+import {ERR_OK} from '@/api/config'
+
+let key = ''
+getPlayKey().then((res) => {
+  if (res.code === ERR_OK) {
+    key = res.key
+  }
+})
+export default class Song {
+  constructor({id, mid, singer, name, album, duration, image, url}){
+    this.id = id;
+    this.mid = mid;
+    this.singer = singer;
+    this.name = name;
+    this.album = album;
+    this.duration = duration;
+    this.image = image;
+    this.url = url;
+  }
+}
+
+export function createSong(musicData) {
+  const playUrl = 'http://dl.stream.qqmusic.qq.com/';
+  return new Song({
+    id: musicData.songid,
+    mid: musicData.songmid,
+    singer: filterSinger(musicData.singer),
+    name: musicData.songname,
+    album: musicData.albumname,
+    duration: musicData.interval,
+    image: `http://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
+    url: playUrl + 'C400' + musicData.songmid + '.m4a?guid=9282438640&vkey=' + key + '&uin=0&fromtag=999'
+  })
+}
+
+
+function filterSinger(singer) {
+  let ret = [];
+  if (!singer) {
+    return ''
+  }
+  singer.forEach((s) => {
+    ret.push(s.name)
+  })
+  return ret.join('/')
+}
